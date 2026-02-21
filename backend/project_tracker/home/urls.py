b907e0ce50c4561d150e0049137c2acd64315c2e
Jobin Jose <jobin.jose@tresvance.com>
2026-02-21 10:53:03 +0530

    updates

--- backend/project_tracker/home/urls.py
+++ backend/project_tracker/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+from .views import ProjectViewSet, TimesheetViewSet
 
 router = DefaultRouter()
 router.register(r'home', ProjectViewSet)
+router.register(r'timesheets', TimesheetViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
